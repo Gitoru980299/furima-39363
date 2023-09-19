435779a49e03ba3f3e1786175939d2d1d4fe5d4d
Gitoru980299 <tmatsumoto299@gmail.com>
2023-09-19 21:51:42 +0900

    21th commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+  #get 'items/index'
+  #get 'furimas/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  root to: "items#index"
- resources :items, only: [:new] do
-  #resources :comments, only: :create
- end
 end
-#, :create, :show, :edit, :update, :destroy
