61c64dbe8287640f6182648aebdd81a2b2c0c7f4
Gitoru980299 <tmatsumoto299@gmail.com>
2023-09-23 20:42:09 +0900

    28th commit
    商品情報編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #get 'furimas/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  root to: 'items#index'
- resources :items, only: [:new, :create, :show] do
+ resources :items, only: [:new, :create, :show, :edit, :update] do
 end
 
  devise_for :users
